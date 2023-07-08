16edee75ef78b3fda42d607c1cb41ea2a2f53aec
amriddin06 <amriddinjorayev7@gmail.com>
2023-07-08 15:54:25 +0300

    1-dars

--- 1-dars/script.js
+++ 1-dars/script.js
@@ -1,9 +1,16 @@
 "use strict";
 
-const favouriteСolor = []
+// const favouriteСolor = []
 
-favouriteСolor[0] = prompt("What's your favorite colo #1" ,"");
-favouriteСolor[1] = prompt("What's your favorite colo #2" ,"");
-favouriteСolor[2] = prompt("What's your favorite colo #3" ,"");
+// favouriteСolor[0] = prompt("What's your favorite colo #1" ,"");
+// favouriteСolor[1] = prompt("What's your favorite colo #2" ,"");
+// favouriteСolor[2] = prompt("What's your favorite colo #3" ,"");
 
-console.log(favouriteСolor);
+// console.log(favouriteСolor); 
+
+// 1-amal
+
+// const channel = prompt("What's your favorite  YouTube")
+// console.log("https://youtube/" + channel)
+
+// 2-amal
