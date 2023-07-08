36a355eea68e71909603398cade7a85034eb4ee1
amriddin06 <amriddinjorayev7@gmail.com>
2023-07-08 14:56:59 +0300

    1-dars

---
+++ 1-dars/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const favouriteСolor = []
+
+favouriteСolor[0] = prompt("What's your favorite colo #1" ,"");
+favouriteСolor[1] = prompt("What's your favorite colo #2" ,"");
+favouriteСolor[2] = prompt("What's your favorite colo #3" ,"");
+
+console.log(favouriteСolor);
